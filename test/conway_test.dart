7b5eb863f045627685f931dbdcc2ca1989e12b56
crystall.101600 <crystall.101600@gmail.com>
2025-03-09 18:36:47 +0000

    Added the tests to cover and verify every condition.

--- test/conway_test.dart
+++ test/conway_test.dart
@@ -6,9 +6,50 @@ void main() {
     final world = WorldState(10, 10);
     expect(world.getDimensions(1, 2), CellState.dead);
     world.setDimensions(1, 2, CellState.alive);
+    //SAVING THE STATE
     expect(world.getDimensions(1, 2), CellState.alive);
-    //OUT OF BOUNDS 
+    expect(world.getDimensions(1, 2), CellState.alive);
+    //CLEARING AFTER FETCHING
+    world.setDimensions(1, 2, CellState.dead);
+    expect(world.getDimensions(1, 2), CellState.dead);
+    //OUT OF BOUNDS
     expect(world.getDimensions(-1, -1), CellState.dead);
+    world.setDimensions(-1, -1, CellState.alive);
+    expect(world.getDimensions(-1, -1), CellState.dead);
+
+    expect(world.getDimensions(-3, 7), CellState.dead);
+    world.setDimensions(-3, 7, CellState.alive);
+    expect(world.getDimensions(-3, 7), CellState.dead);
+
+    expect(world.getDimensions(3, -7), CellState.dead);
+    world.setDimensions(3, -7, CellState.alive);
+    expect(world.getDimensions(3, -7), CellState.dead);
+
     expect(world.getDimensions(11, 12), CellState.dead);
+    world.setDimensions(11, 12, CellState.alive);
+    expect(world.getDimensions(11, 12), CellState.dead);
+  });
+
+  test('All alive boundary test', () {
+    //CHECKING FOR WIDTH/HEIGHT OFF-BY ONE
+    //Creating new object
+    final world = WorldState(10, 10)
+    ..setAll(CellState.alive);
+
+    expect(world.getDimensions(9, 7), CellState.alive);
+    expect(world.getDimensions(8, 7), CellState.alive);
+
+    expect(world.getDimensions(10, 7), CellState.dead);
+    expect(world.getDimensions(11, 7), CellState.dead);
+    expect(world.getDimensions(7, 10), CellState.dead);
+    expect(world.getDimensions(7, 11), CellState.dead);
+  });
+
+  test('Rectangular test', () {
+    //CHECK THE SWAPPING OF WIDTH AND HEIGHT
+    //Creating a new object
+    final world = WorldState(5, 10);
+    expect(world.getDimensions(6, 9), CellState.dead);
+    expect(world.getDimensions(9, 6), CellState.dead);
   });
 }
