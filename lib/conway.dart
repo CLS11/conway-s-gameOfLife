48c540fe452aeb9117b50e9054204a2b48f7aa4f
crystall.101600 <crystall.101600@gmail.com>
2025-03-10 12:21:42 +0000

    Added methods significant to the rules of the game.

--- lib/conway.dart
+++ lib/conway.dart
@@ -2,6 +2,8 @@ import 'dart:typed_data';
 
 enum CellState { dead, alive }
 
+typedef WorldStateCallback = Function(int x, int y, CellState value);
+
 class WorldState {
   WorldState(this.height, this.width) {
     _data = Uint8List(width * height);
@@ -33,8 +35,57 @@ class WorldState {
     }
     _data[x + width * y] = value.index;
   }
+
+  void forEach(WorldStateCallback callback) {
+    for (var x = 0; x < width; ++x) {
+      for (var y = 0; y < height; ++y) {}
+    }
+  }
+
+  //CHECKING THE ALIVE NEIGHBOURHOOD
+  int countAliveNeighbors(int x, int y) {
+    var count = 0;
+    for (var dx = -1; dx <= 1; ++dx) {
+      for (var dy = -1; dy <= 1; ++dy) {
+        if (dx == 0 && dy == 0) {
+          continue;
+        }
+        if (getDimensions(x + dx, y + dy) == CellState.alive) {
+          ++count;
+        }
+      }
+    }
+    return count;
+  }
+
+  int countAlive() {
+    var count = 0;
+    forEach((int x, int y, CellState value) {
+      if (value == CellState.alive) {
+        ++count;
+      }
+    });
+    return count;
+  }
 }
 
-WorldState? next(WorldState old) {
+WorldState? next(WorldState oldWorld) {
+  final newWorld = WorldState(oldWorld.width, oldWorld.height);
+
+  oldWorld.forEach((int x, int y, CellState value) {
+    final aliveNeighbor = oldWorld.countAliveNeighbors(x, y);
+    //UNDERPOPULATION CONDITION: FEWER NEIGHBOURS THAN 2 LIVE CELL
+    //=> LIVE CELL DIES
+    if (value == CellState.alive) {
+      if (aliveNeighbor >= 2) {
+        newWorld.setDimensions(x, y, CellState.alive);
+      }
+    } else {
+      //DEAD CELL => ALIVE WHEN 3 NEIGHBOURS ARE ALIVE
+      if (aliveNeighbor >= 3) {
+        newWorld.setDimensions(x, y, CellState.alive);
+      }
+    }
+  });
   return null;
 }
