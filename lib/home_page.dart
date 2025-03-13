8150e8beb18dacd67e63733646af010b95a11d54
crystall.101600 <crystall.101600@gmail.com>
2025-03-13 12:55:24 +0000

    Added cell position and changed the UI.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:conway/cell_position.dart';
 import 'package:conway/conway.dart';
 import 'package:conway/conway_game.dart';
 import 'package:flutter/material.dart';
@@ -24,12 +25,16 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     world = WorldState.fromFixture('''
-......
-.xx...
-.xx...
-...xx.
-...xx.
-......
+........................
+.xx....xx....xx....xx...
+.xx....xx....xx....xx...
+...xx....xx....xx....xx.
+...xx....xx....xx....xx.
+........................
+........................
+........................
+........................
+........................
 ''');
   }
 
@@ -37,10 +42,28 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text(widget.title))),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [Expanded(child: ConwayGame(world: world))],
-      ),
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.black12,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                 child: ConwayGame(
+                  world: world,
+                  onToggle: (CellPosition position) {
+                    setState(() {
+                      world.toggle(position);
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       floatingActionButton: FloatingActionButton(
         onPressed: _increementWorld,
         tooltip: 'NEXT',
